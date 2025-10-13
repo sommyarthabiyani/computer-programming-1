a6cc7ebad3574379f9759adfc2252c0639266cca
sommyarthabiyani <sommyarthabiyaniemail@gmail.com>
2025-10-13 12:26:25 +0530

    Add files via upload

---
+++ div_with rv no arg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+
+{
+    float c;
+    float add (void);
+    c= add();
+    printf("answer=%f",c);
+}
+    float add(void)
+
+{
+        float x,y,z;
+         printf("\nEnter 2 nos.");
+          scanf("%f %f",&x,&y);
+           z= x/y;
+           return z;
+}
+
+
+
