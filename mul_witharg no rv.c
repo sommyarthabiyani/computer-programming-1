ed7dad37b784a11b7ec31c7e92b3d3086821118b
sommyarthabiyani <sommyarthabiyaniemail@gmail.com>
2025-10-13 12:09:39 +0530

    Add files via upload

---
+++ mul_witharg no rv.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+
+{
+    int a,b;
+    printf("\nEnter 2 nos.");
+    scanf("%d %d",&a,&b);
+    add (a,b);
+}
+    void add(int x,int y)
+    {
+        int z;
+        z= x*y;
+    printf("answer=%d",z);
+}
+
+
